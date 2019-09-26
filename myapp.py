4b5c0c4cc4cb53626474d8a05f383435138745a2
Justin Sobieski <jmksobieski@gmail.com>
2019-09-26 08:54:57 -0400

    added minimal logging, possible fix for json serializable

--- myapp.py
+++ myapp.py
@@ -5,6 +5,19 @@ from flask_bootstrap import Bootstrap
 import pytz
 from datetime import datetime
 import json
+import logging
+
+
+def add_log(the_type, string_to_log):
+    today = datetime.today()
+    day = today.strftime("%d%m%y")
+    logging.basicConfig(filename=f'{day}.log', level=logging.DEBUG)
+    if the_type == 'info':
+        logging.info(f' {string_to_log}')
+    if the_type == 'warning':
+        logging.warning(f' {string_to_log}')
+    if the_type == 'debug':
+        logging.debug(f' {string_to_log}')
 
 
 def datetimefilter(value, theformat="%d/%m/%y %I:%M %p"):
@@ -66,15 +79,25 @@ def apimembersreset():
 def apimembers():
     if request.headers.get("X-Api-Key") == api_key:
         if request.method == "GET":
+            add_log('info', '/api/member GET received')
             all_mems = Member.query.order_by(Member.Level.desc()).all()
             the_dict = {}
             for item in all_mems:
                 the_dict[item.TornID] = item.dict_info()
+            add_log('info', f'/api/member GET sent {jsonify(the_dict)}')
             return jsonify(the_dict), 200
 
         if request.method == "PUT":
-            to_json = json.loads(request.data.decode("utf-8").replace("'", '"'))
+            try:
+                add_log('info', '/api/member PUT received')
+                to_json = json.loads(request.data)
+            except Exception as e:
+                exc = f'{type(e).__name__}: {e}'
+                add_log('warning', f'/api/member PUT failed to load json {exc}')
+                return jsonify(e)
+
             #print(to_json)
+            add_log('info', '/api/member PUT json passed')
             for member in to_json:
                 refills = 0
                 xan = 0
@@ -105,12 +128,17 @@ def apimembers():
                 if not user:
                     test = Member(LastSeen=last_seen, TornID=member, Name=to_json[member]['name'], Rank=to_json[member]['rank'], Level=to_json[member]['level'], Age=to_json[member]['age'], Refills=refills, Xan=xan, XanThisMonth=xanthismonth, LSD=lsd, StatEnhancers=se)
                     db_session.add(test)
-                    db_session.commit()
+                    try:
+                        add_log('info', f'/api/member PUT adding member {test}')
+                        db_session.commit()
+                    except Exception as e:
+                        add_log('warning', f'/api/member PUT adding member {test} failed {e}')
+                        db_session.rollback()
+
                 if user:
                     if xan != 0:
                         if xan > user.Xan:
                             user.XanThisMonth = user.XanThisMonth + (xan - user.Xan)
-                            print(user, user.XanThisMonth)
 
                     user.LastSeen = last_seen
                     user.Name = to_json[member]['name']
@@ -122,9 +150,16 @@ def apimembers():
                     user.XanThisMonth = user.XanThisMonth
                     user.LSD = lsd
                     user.StatEnhancers = se
-                    db_session.commit()
+                    try:
+                        add_log('info', f'/api/member PUT updating member {user.Name}')
+                        db_session.commit()
+                    except Exception as e:
+                        add_log('warning', f'/api/member PUT updating member {user.Name} failed {e}')
+                        db_session.rollback()
+            add_log('info', f'/api/member PUT completed')
             return "members done", 200
     else:
+        add_log('warning', f'/api/member PUT unauthorized attempt')
         return jsonify({"message": "ERROR: Unauthorized"}), 401
 
 
@@ -132,6 +167,7 @@ def apimembers():
 def apirackets():
     if request.headers.get("X-Api-Key") == api_key:
         if request.method == "GET":
+            add_log('info', '/api/racket GET received')
             get_rackets = Racket.query.order_by(Racket.Level.desc()).all()
             the_dict = {}
             for item in get_rackets:
@@ -140,6 +176,7 @@ def apirackets():
 
         if request.headers.get("X-Api-Key") == api_key:
             if request.method == "PUT":
+                add_log('info', '/api/racket PUT received')
                 to_json = json.loads(request.data.decode("utf-8").replace("'", '"'))
                 input_list = [racket for racket in to_json]
                 the_rackets = Racket.query.order_by(Racket.Level.desc()).all()
@@ -156,14 +193,24 @@ def apirackets():
                         racket_to_update.Owner = to_json[racket]['faction']
                         racket_to_update.OwnerName = to_json[racket]['factionname']
                         racket_to_update.Level = to_json[racket]['level']
-                db_session.commit()
+                try:
+                    add_log('info', f'/api/rackets PUT updating rackets')
+                    db_session.commit()
+                except Exception as e:
+                    add_log('warning', f'/api/rackets PUT updating rackets failed {e}')
+                    db_session.rollback()
                 for racket in input_list:
                     if racket not in db_list:
                         new_racket = Racket(TerritoryName=racket, RacketName=to_json[racket]['name'], Reward=to_json[racket]['reward'],
                                       Created=to_json[racket]['created'], Changed=to_json[racket]['changed'],
                                       Owner=to_json[racket]['faction'], OwnerName=to_json[racket]['factionname'], Level=to_json[racket]['level'])
                         db_session.add(new_racket)
-                        db_session.commit()
+                        try:
+                            add_log('info', f'/api/rackets PUT updating rackets')
+                            db_session.commit()
+                        except Exception as e:
+                            add_log('warning', f'/api/rackets PUT updating rackets failed {e}')
+                            db_session.rollback()
                 return "rackets done", 200
     else:
         return jsonify({"message": "ERROR: Unauthorized"}), 401
