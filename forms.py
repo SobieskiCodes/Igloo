378c96830769e5a101b078a72905a532e361078c
Justin Sobieski <jmksobieski@gmail.com>
2019-11-14 12:53:01 -0500

    stop relying on forms for this crap justin.

--- forms.py
+++ forms.py
@@ -1,12 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField
+from wtforms import StringField
 from wtforms.validators import DataRequired, Length
-import os
-
-
-class ReportSelect(FlaskForm):
-    Report = SelectField('Select Report', choices=list((x[:-5], x[:-5]) for x in os.listdir("./reports") if x.endswith('.json')))
-
 
 class FactionIDForm(FlaskForm):
     WarbaseFaction = StringField('FactionID', validators=[DataRequired(), Length(min=-1, max=20,
