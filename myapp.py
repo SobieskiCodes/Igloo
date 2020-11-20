9dba67fcb6bdacd03d5bbc5a7074a4cee6233ad2
Justin Sobieski <justin@sobieski.codes>
2020-11-20 14:42:32 -0500

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify, flash, redirect, url_for, send_file
 from database import db_session
-from models import Racket, Member, WarBase, Settings
+from models import Racket, Member, WarBase, Settings, Company, Employees, OCs
 from flask_bootstrap import Bootstrap
 import pytz
 from datetime import datetime
@@ -13,6 +13,11 @@ from werkzeug.utils import secure_filename
 import requests
 import pandas as pd
 from pathlib import Path
+from configparser import ConfigParser
+parser = ConfigParser()
+parser.read('config.ini')
+torn_key = parser.get('secrets', 'torn_api_key')
+api_key = parser.get('secrets', 'api_header_key')
 
 
 def datetimefilter(value, theformat="%d/%m/%y %I:%M %p"):
@@ -30,7 +35,6 @@ app.secret_key = 'dev'
 app.config['DEBUG'] = False
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 bootstrap = Bootstrap(app)
-api_key = os.environ.get('api_header_key')
 UPLOAD_FOLDER = './csvs/'
 ALLOWED_EXTENSIONS = ['csv']
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -45,6 +49,106 @@ def shutdown_session(exception=None):
 def index():
     get_rackets = Racket.query.order_by(Racket.Level.desc()).all()
     return render_template('index.html', rackets=get_rackets)
+
+
+@app.route('/t', methods=["GET"])
+def test123123213213():
+    flash('hello world', 'danger')
+    return render_template('test.html')
+
+
+@app.route('/companies', methods=["GET", "POST"])
+def test123():
+    if request.method == "POST":
+        add = request.form.get('companyid')
+        remove = request.form.get('deletecompany')
+        if add:
+            print('addding', add)
+            url = f"https://api.torn.com/company/{add}?selections=&key={torn_key}"
+            try:
+                get_company = requests.get(url).json()
+                if get_company.get('error'):
+                    flash(f'Torn says: {get_company.get("error").get("error")}', 'danger')
+                db_query = Company.query.filter_by(ID=add).first()
+                if db_query:
+                    flash(f'Company {add} already exists in the database.', 'danger')
+                if not get_company.get('error') and not db_query:
+                    company = get_company.get('company')
+                    status = "success"
+                    for employee in company.get('employees'):
+                        add_employee = Employees(CompanyID=add,
+                                                 LastSeen=company.get('employees').get(employee).get("last_action").get(
+                                                     'relative'),
+                                                 ID=employee,
+                                                 EmployeeName=company.get('employees').get(employee).get('name'),
+                                                 Position=company.get('employees').get(employee).get('position'))
+                        for word in ['days', 'day']:
+                            if word in company.get('employees').get(employee).get("last_action").get('relative'):
+                                status = "danger"
+                        db_session.add(add_employee)
+                    test = Company(ID=add, CompanyName=company.get('name'), Weekly=company.get('weekly_profit'),
+                                   Daily=company.get('daily_profit'), Status=status, Rank=company.get('rating'))
+                    db_session.add(test)
+                    db_session.commit()
+                    flash(f'Company {add} successfully added to the database.', 'success')
+            except Exception as e:
+                flash(f'Error: {e}', 'danger')
+                return render_template('companies.html')
+
+        if remove and remove != "Empty":
+            db_query = Company.query.filter_by(ID=remove).first()
+            db_session.delete(db_query)
+            fetch_employees = Employees.query.filter_by(CompanyID=remove).all()
+            for employee in fetch_employees:
+                db_session.delete(employee)
+            db_session.commit()
+            flash(f'Successfully removed {remove}', 'success')
+        return render_template('companies.html', companies=Company.query.order_by(Company.ID.desc()).all())
+
+    if request.method == "GET":
+        return render_template('companies.html', companies=Company.query.order_by(Company.ID.desc()).all())
+
+
+@app.route('/employees', methods=["GET", "POST"])
+def test222():
+    companyid = request.args.get('companyid', default=1, type=str)
+    db_query = Company.query.filter_by(ID=companyid).first()
+    if db_query:
+        return render_template('employees.html', employees=Employees.query.filter_by(CompanyID=companyid).all(),
+                               title=db_query.CompanyName)
+    else:
+        flash(f"Company {companyid} doesn't exist here")
+        return render_template('employees.html')
+
+
+@app.route('/organizedcrime', methods=["GET", "POST"])
+def test233():
+    if request.method == "GET":
+        db_query = OCs.query.order_by(OCs.CE).all()
+        return render_template('oc.html', people=db_query)
+
+    if request.method == "POST":
+        print(request.form.get('id'))
+        for k,v in request.form.items():
+            print(k,v )
+        try:
+            get_person = OCs.query.filter_by(ID=request.form.get('id')).first()
+            if get_person:
+                to_update = request.form.get('key')
+                if to_update == "PersonalNote":
+                    get_person.PersonalNote = request.form.get('data')
+                if to_update == "OtherNote":
+                    get_person.OtherNote = request.form.get('data')
+                if to_update == "CE":
+                    get_person.CE = request.form.get('data')
+                db_session.commit()
+                return jsonify({"message": get_person.dict_info()}), 200
+            else:
+                return jsonify({"Error": f"{request.form.get('id')} not found"}), 404
+
+        except Exception as e:
+            return jsonify({"Error": f"{e}"}), 500
+
 
 
 def allowed_file(filename):
@@ -75,6 +179,7 @@ def list_reports():
 @app.route("/tools", methods=["GET", "POST"])
 def thetool():
     if request.method == "GET":
+        flash('This page is deprecated and no longer maintained. It is merely here for preservation.', 'danger')
         return render_template('tools.html', beforeform=make_the_fucking_csv(), afterform=make_the_fucking_csv())
 
     if request.method == "POST":
@@ -99,7 +204,7 @@ def thetool():
                               sort=True, axis=1))
                 new = new.loc[:, ~new.columns.duplicated()]
                 new['Difference'] = new.apply(lambda x: x['After'] - x['Before'], axis=1)
-                faction_url = f"https://api.torn.com/faction/?selections=&key={os.environ.get('torn_api_key')}"
+                faction_url = f"https://api.torn.com/faction/?selections=&key={torn_key}"
                 test = requests.get(faction_url)
                 members = test.json()
                 list_mems = list(members['members'][x]['name'] for x in members['members'])
@@ -132,7 +237,7 @@ def build_report(filename):
     ids = ['graph-{}'.format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
     kwargs = [dict(x=list(data.keys()), y=list(item.get("y") for item in data.values()), type="bar")]
-    #print(kwargs)
+    # print(kwargs)
     return ids, graphJSON
 
 
@@ -154,6 +259,57 @@ def members():
     return render_template('members.html', members=get_members)
 
 
+class Enemy:
+    def __init__(self, fac, id, nm, rk, lvl, ag, ref, xan, lsd, se, aw, logins):
+        self.faction = fac
+        self.userid = id
+        self.name = nm
+        self.rank = rk
+        self.level = lvl
+        self.age = ag
+        self.refills = ref
+        self.xan = xan
+        self.lsd = lsd
+        self.se = se
+        self.attacks_won = aw
+        self.logins = logins
+
+
+@app.route('/war', methods=['GET'])
+def thisiswar():
+    if request.method == "GET":
+        flash('This page is deprecated and no longer maintained. It is merely here for preservation.', 'danger')
+        with open('mergethem.json', 'r') as stuff:
+            data = json.load(stuff)
+        users = []
+        for faction in data:
+            for member in data[faction]:
+                fac = data[faction][member].get('faction').get('faction_name')
+                id = data[faction][member].get('player_id')
+                name = data[faction][member].get('name')
+                rank = data[faction][member].get('rank')
+                level = data[faction][member].get('level')
+                age = data[faction][member].get('age')
+                refills = data[faction][member].get('personalstats').get('refills')
+                xan = data[faction][member].get('personalstats').get('xantaken')
+                lsd = data[faction][member].get('personalstats').get('lsdtaken')
+                se = data[faction][member].get('personalstats').get('statenhancersused')
+                attacks = data[faction][member].get('personalstats').get('attackswon')
+                logins = data[faction][member].get('personalstats').get('logins')
+                if not refills:
+                    refills = 0
+                if not xan:
+                    xan = 0
+                if not lsd:
+                    lsd = 0
+                if not se:
+                    se = 0
+                if not attacks:
+                    attacks = 0
+                users.append(Enemy(fac, id, name, rank, level, age, refills, xan, lsd, se, attacks, logins))
+        return render_template('war.html', members=users)
+
+
 @app.route('/warbase', methods=['GET', 'POST'])
 def thewarbase():
     db_query = Settings.query.order_by(Settings.WarbaseFaction).first()
@@ -168,10 +324,8 @@ def thewarbase():
                     form = Settings(WarbaseFaction=get_input)
                     db_session.add(form)
                     db_session.commit()
-                    flash('Saved successfully', 'success')
+                    flash('Saved successfully, you may now hit update.', 'success')
                 if db_query:
-                    print(type(db_query.WarbaseFaction))
-                    print(type(get_input))
                     if str(db_query.WarbaseFaction) != str(get_input):
                         db_query.WarbaseFaction = get_input
                         all_mems = WarBase.query.order_by(WarBase.Level.desc()).all()
@@ -179,11 +333,14 @@ def thewarbase():
                             db_session.delete(person)
                         db_session.commit()
                         get_members = WarBase.query.order_by(WarBase.Level.desc()).all()
-            except Exception as e:
-                print(e)
-                db_session.rollback()
-                flash('Error updating faction.', 'danger')
+                        flash('Saved successfully, you may now hit update.', 'success')
+                    else:
+                        flash(f'{get_input} is already the faction saved, hit update.', 'danger')
 
+            except Exception as e:
+                db_session.rollback()
+                flash(f'Error updating faction {e}.', 'danger')
+                return render_template('warbase.html', members=get_members, form=the_form)
             return render_template('warbase.html', members=get_members, form=the_form)
 
     if request.method == "GET":
@@ -196,16 +353,86 @@ def runupdate():
         logging.info('GET /runupdate received')
         db_query = Settings.query.order_by(Settings.WarbaseFaction).first()
         if db_query:
-            process = subprocess.Popen(['python', 'updatewarbase.py', f"{db_query.WarbaseFaction}"])
-            logging.info('GET /runupdate started')
+            try:
+                faction_url = f"https://api.torn.com/faction/{db_query.WarbaseFaction}?selections=&key={torn_key}"
+                get_fact = requests.get(faction_url)
+                print(get_fact.json())
+                if "error" not in get_fact.json().keys():
+                    process = subprocess.Popen(['python', 'updatewarbase.py', f"{db_query.WarbaseFaction}"])
+                    logging.info('GET /runupdate started')
+                    flash('Updating now, please be patient, this could take up to 10 minutes.', 'success')
+                else:
+                    flash(f"torn says: {get_fact.json().get('error')}", "danger")
+            except Exception as e:
+                flash(f"{e}", "danger")
+                return redirect(url_for('thewarbase'))
+        else:
+            flash(f"no db entry?", "danger")
+            return redirect(url_for('thewarbase'))
         return redirect(url_for('thewarbase'))
 
 
 @app.route("/api/test", methods=['GET'])
-def apitest():
+def api_test():
     if request.headers.get("X-Api-Key") == api_key:
         logging.info('GET /api/test received')
         return jsonify({"message": "OK"}), 200
+    else:
+        return jsonify({"message": "ERROR: Unauthorized"}), 401
+
+
+@app.route("/api/companies", methods=['GET', 'PUT'])
+def apicoids():
+    if request.headers.get("X-Api-Key") == api_key:
+        if request.method == "GET":
+            logging.info('GET /api/test received')
+            return jsonify({"ids": [company.ID for company in Company.query.order_by(Company.ID.desc()).all()]})
+
+        if request.method == "PUT":
+            try:
+                logging.info('/api/companies PUT received')
+                to_json = json.loads(request.data)
+            except Exception as e:
+                exc = f'{type(e).__name__}: {e}'
+                logging.warning(f'/api/companies PUT failed to load json {exc}')
+                return jsonify(e)
+
+            for co, v in to_json.items():
+                status = "success"
+                employees_list = []
+                get_co = Company.query.filter_by(ID=to_json[co]['ID']).first()
+                get_co.Weekly = to_json[co]['weekly_profit']
+                get_co.Daily = to_json[co]['daily_profit']
+                get_co.Rank = to_json[co]['rating']
+                for member in to_json[co]['employees']:
+                    employees_list.append(member)
+                    get_mem = Employees.query.filter_by(ID=member).first()
+                    for word in ['days', 'day']:
+                        if word in to_json[co]['employees'][member]['last_action']['relative']:
+                            status = "danger"
+                    if get_mem:
+                        get_mem.EmployeeName = to_json[co]['employees'][member]['name']
+                        get_mem.LastSeen = to_json[co]['employees'][member]['last_action']['relative']
+                        get_mem.Position = to_json[co]['employees'][member]['position']
+
+                    if not get_mem:
+                        add_employee = Employees(CompanyID=co,
+                                                 LastSeen=to_json[co]['employees'][member]['last_action']['relative'],
+                                                 ID=member,
+                                                 EmployeeName=to_json[co]['employees'][member]['name'],
+                                                 Position=to_json[co]['employees'][member]['position']
+                                                 )
+                        db_session.add(add_employee)
+                co_list = [str(emp.ID) for emp in Employees.query.filter_by(CompanyID=co).all()]
+                for person in co_list:
+                    if person not in employees_list:
+                        get_pers = Employees.query.filter_by(ID=person).first()
+                        db_session.delete(get_pers)
+                get_co.Status = status
+            db_session.commit()
+            return jsonify({"message": "Companies updated"}), 200
+        else:
+            return jsonify({"message": "ERROR: Unauthorized"}), 401
     else:
         return jsonify({"message": "ERROR: Unauthorized"}), 401
 
@@ -313,7 +540,7 @@ def apimembersclean():
                 for item in all_members:
                     item.Status = '0'
                     get_db[item.TornID] = item.dict_info()
-                faction_url = f"https://api.torn.com/faction/?selections=&key={os.environ.get('torn_api_key')}"
+                faction_url = f"https://api.torn.com/faction/?selections=&key={torn_key}"
                 test = requests.get(faction_url)
                 get_fac = test.json()['members']
                 logging.info(f'/api/members/clean POST get db{get_db}')
@@ -464,7 +691,7 @@ def apiwarbase():
 
                 user = WarBase.query.filter_by(TornID=member).first()
                 if not user:
-                    test = WarBase(LastSeen=last_seen, Status=to_json[member]['status'][0], TornID=member,
+                    test = WarBase(LastSeen=last_seen, Status=to_json[member]['status']['description'], TornID=member,
                                    Name=to_json[member]['name'], Rank=to_json[member]['rank'],
                                    Level=to_json[member]['level'], Age=to_json[member]['age'], Refills=refills, Xan=xan,
                                    LSD=lsd, StatEnhancers=se)
@@ -478,7 +705,7 @@ def apiwarbase():
 
                 if user:
                     user.LastSeen = last_seen
-                    user.Status = to_json[member]['status'][0]
+                    user.Status = to_json[member]['status']['description']
                     user.Name = to_json[member]['name']
                     user.Rank = to_json[member]['rank']
                     user.Level = to_json[member]['level']
