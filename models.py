a4081bf0cf9e8437ce83e9da9203698e424f21e7
Justin Sobieski <justin@sobieski.codes>
2020-12-10 10:27:19 -0500

    add fac id

--- models.py
+++ models.py
@@ -50,6 +50,7 @@ class Member(Base):
     XanThisMonth = Column(String(200), nullable=True)
     LSD = Column(String(200), nullable=True)
     StatEnhancers = Column(String(200), nullable=True)
+    Fac = Column(String(200), nullable=True)
 
     def __repr__(self):
         return f'<Member: {self.Name} [{self.TornID}]>'
@@ -66,7 +67,8 @@ class Member(Base):
             Xan=self.Xan,
             XanThisMonth=self.XanThisMonth,
             LSD=self.LSD,
-            StatEnhancers=self.StatEnhancers
+            StatEnhancers=self.StatEnhancersc,
+            Fac=self.Fac
         )
 
 class WarBase(Base):
@@ -106,3 +108,38 @@ class Settings(Base):
 
     def __repr__(self):
         return f'<Settings: {self.WarbaseFaction}>'
+class Company(Base):
+    __tablename__ = 'Companies'
+    ID = Column(String(200), primary_key=True)
+    CompanyName = Column(String(200), nullable=True)
+    Weekly = Column(String(200), nullable=True)
+    Daily = Column(String(200), nullable=True)
+    Status = Column(String(200), nullable=True)
+    Rank = Column(String(200), nullable=True)
+
+
+class Employees(Base):
+    __tablename__ = 'Employee'
+    CompanyID = Column(String(200), nullable=True)
+    LastSeen = Column(String(200), nullable=True)
+    ID = Column(String(200), primary_key=True)
+    EmployeeName = Column(String(200), nullable=True)
+    Position = Column(String(200), nullable=True)
+
+
+class OCs(Base):
+    __tablename__ = 'OC'
+    ID = Column(String(200), primary_key=True)
+    Name = Column(String(200), nullable=True)
+    CE = Column(String(200), nullable=True)
+    PersonalNote = Column(String(200), nullable=True)
+    OtherNote = Column(String(200), nullable=True)
+
+    def dict_info(self):
+        return dict(
+            ID=self.ID,
+            Name=self.Name,
+            CE=self.CE,
+            PersonalNote=self.PersonalNote,
+            OtherNote=self.OtherNote,
+        )
